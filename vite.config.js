eca2948dd0966ca044e1ef858379febb46effa2e
Deise Wapinda <euvenancio1@gmail.com>
2023-04-15 00:52:50 +0100

    Vercel segunda correção de erro de bilde

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,13 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
-    build: {
-        rollupOptions: {
-            external: ['alpinejs']
-        }
-    }
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+  ],
+  build: {
+    outDir: 'public/build' // define o diretório de saída como public/build
+  }
 });
